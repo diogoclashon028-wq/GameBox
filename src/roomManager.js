d394255512b467eb4528c6454cfe2118e4407baa
diogoclashon028-wq <diogoclashon028@gmail.com>
2026-07-12 00:27:17 -0300

    Remove comments from RoomManager methods

--- src/roomManager.js
+++ src/roomManager.js
@@ -1,12 +1,10 @@
 const activeRooms = new Map();
 
 class RoomManager {
-  // Verifica se o canal já tem um jogo rodando
   hasGame(channelId) {
     return activeRooms.has(channelId);
   }
 
-  // Cria uma nova janela de jogo no canal
   createRoom(channelId, gameName, players) {
     activeRooms.set(channelId, {
       game: gameName,
@@ -15,11 +13,9 @@ class RoomManager {
     });
   }
 
-  // Finaliza o jogo e libera o canal para novas janelas
   destroyRoom(channelId) {
     activeRooms.delete(channelId);
   }
 }
 
 module.exports = new RoomManager();
-
